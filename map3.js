8740ac57ed05561b95475de3a179f33f65f69ac8
sumedh-shingade <44223852+sumedh-shingade@users.noreply.github.com>
2023-05-07 20:03:49 +0530

    Update map3.js

--- map3.js
+++ map3.js
@@ -1,2 +1,5 @@
+
+// using map and lambda function show first two letter of each string to uppercase
+
 arr = ["sumedh", "kunal", "swapnil", "john"];
 console.log(arr.map((e) => e.slice(0, 2).toUpperCase()));
