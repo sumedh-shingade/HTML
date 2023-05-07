982feb5a189437d724010bbcd04215241494bfa8
sumedh-shingade <44223852+sumedh-shingade@users.noreply.github.com>
2023-05-07 20:15:57 +0530

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,5 +1,16 @@
+//write a js program to print odd and even numbers from array 
+
+
 arr = [45, 87, 32, 15, 21, 54];
 let oddnumbers = arr.filter((e) => {
   return e % 2 != 0;
 });
+
+// for even
+
+
+/*
+let oddnumbers = arr.filter((e) => {
+  return e % 2 != 0;
+});*/
 console.log(oddnumbers);
